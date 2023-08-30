da696c3820f9e11b044a7048e6a968c4af21d407
nhinguyen <nguyenhis@gmail.com>
2023-08-30 11:58:05 +0300

    2_3 commit

---
+++ main.py
@@ -0,0 +1,5 @@
+width = float(input("Enter value of width: "))
+length = float(input("Enter value of length: "))
+perimeter = (width*2) + (length*2)
+area = width*length
+print(f"Area of the rectangle is: {area:.2f}"/n"Perimeter of the rectangle is: {")
